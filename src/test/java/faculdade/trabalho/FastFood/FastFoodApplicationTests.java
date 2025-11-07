3d95100384514adeb6125d683282453ee7bfa593
lipwns <filipe.nsantos22@outlook.com>
2025-11-07 16:26:28 -0300

    first commit

---
+++ src/test/java/faculdade/trabalho/FastFood/FastFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package faculdade.trabalho.FastFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
