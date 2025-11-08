3db270c4f9102afadd7aa3283dbaa8376edb36f9
lipwns <filipe.nsantos22@outlook.com>
2025-11-07 23:21:36 -0300

    Cria uma entidade no banco de dados a partir do FuncionarioModel

--- src/main/java/faculdade/trabalho/FastFood/FuncionarioController.java
+++ src/main/java/faculdade/trabalho/FastFood/FuncionarioController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class FuncionarioController {
 
     @GetMapping("/primeiramensagem")
     public String primeiraMensagem(){
